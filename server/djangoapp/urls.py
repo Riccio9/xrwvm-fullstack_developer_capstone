fff0a6ea7c56a88372d78c0f80f970d36c3f8d1d
Riccio9 <andrea.ricci94@gmail.com>
2025-01-31 16:55:25 +0100

    SecondPart

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,8 +1,9 @@
-# Uncomment the imports before you add the code
-# from django.urls import path
+from django.views.generic import TemplateView
+
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
@@ -10,6 +11,12 @@ urlpatterns = [
 
     # path for login
     # path(route='login', view=views.login_user, name='login'),
+        path(route='login', view=views.login_user, name='login'),
+    # path for logout
+        path(route='logout', view=views.logout_request, name='logout'),
+    # path for registration
+        path(route='registration', view=views.registration, name='registration'),
+     
 
     # path for dealer reviews view
 
